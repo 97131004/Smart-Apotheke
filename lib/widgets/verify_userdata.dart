c046751b7da2b0904f6434363d257c71311f98cb
michaelef123 <s0545163@htw-berlin.de>
2019-12-14 18:40:52 +0100

    - worked on confirmation page

--- lib/widgets/verify_userdata.dart
+++ lib/widgets/verify_userdata.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:grouped_buttons/grouped_buttons.dart';
 import 'package:maph_group3/util/nampr.dart';
 import 'package:maph_group3/util/personaldata.dart';
 import 'package:maph_group3/util/shop_items.dart';
@@ -17,15 +19,13 @@ class VerificationUserData extends StatefulWidget {
 
 class _VerificationUserDataState extends State<VerificationUserData> {
 
+  bool agbIsChecked = false;
+  bool offersIsChecked = false;
+
   @override
-  void initState() {
+  initState() {
     super.initState();
-    /*if(!PersonalData.isUserDataComplete()) {
-      Navigator.push(context, NoAnimationMaterialPageRoute(builder: (context) => Personal()));
-    } else {
-      String name = widget.item.name;
-      print("data is ok. display data to verify || name: " + name);
-    }*/
+    _checkData();
   }
 
   @override
@@ -37,9 +37,103 @@ class _VerificationUserDataState extends State<VerificationUserData> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
+            _buildProductOverview(),
+            _buildPaymentOptions(),
+            _buildShippingOptions(),
+            _buildConfirmationContainer(),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildProductOverview() {
+    return new Container(
+      child: Card(
+
+      ),
+    );
+  }
+
+  Widget _buildPaymentOptions() {
+    return new Container();
+  }
+
+  Widget _buildShippingOptions() {
+    return new Container(
+      child: Column(
+        children: <Widget>[
+          Text('Liefermöglichkeiten:'),
+          RadioButtonGroup(
+              labels: <String>[
+                'Nach Hause liefern lassen',
+                'An Apotheke liefern lassen',
+              ],
+              onSelected: (String selected) => print(selected)
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBillingAdress() {
+    return new Container();
+  }
+
+  Widget _buildGooglemapsContainer() {
+    return new Container();
+  }
+
+  Widget _buildConfirmationContainer() {
+    return new Container(
+      child: Column(
+        children: <Widget>[
+          Text("Confirmation"),
+          CheckboxGroup(
+            labels: <String>[
+              'AGBs zustimmen',
+              'Ich bin damit einverstanden, dass...',
+            ],
+            onSelected: (List<String> checked) => print(checked.toString())
+          ),
+        ],
+      ),
+    );
+  }
+
+  _checkData() {
+    if(!PersonalData.isUserDataComplete()) {
+      SchedulerBinding.instance.addPostFrameCallback((_) async {
+        await _dataIncompleteAlert(context);
+        Navigator.push(
+          context,
+          NoAnimationMaterialPageRoute(
+              builder: (context) => Personal()),
+        );
+      });
+    } else {
+      String name = widget.item.name;
+      print("data is ok. display data to verify || name: " + name);
+    }
+  }
+
+  Future<void> _dataIncompleteAlert(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Daten unvollständig'),
+          content: const Text('Ihre persönlichen Daten sind nicht vollständig. Bitte überprüfen und ergänzen.'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
